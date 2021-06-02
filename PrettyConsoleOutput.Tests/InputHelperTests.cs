5e321bbf537e7c6fbb7c8aa34f44040058e0ffe2
chrisk <christian.khalil200@gmail.com>
2021-06-02 20:26:39 +0200

    get enuminput test

---
+++ PrettyConsoleOutput.Tests/InputHelperTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+
+namespace PrettyConsoleOutput.Tests
+{
+    public class InputHelperTests
+    {
+        [Fact]
+        public void GetEnumInput_ThrowsArgumentException_When_SentInType_IsNotAnEnum()
+        {
+            FluentActions.Invoking(() => InputHelper.GetEnumInput<int>());
+        }
+    }
+}
