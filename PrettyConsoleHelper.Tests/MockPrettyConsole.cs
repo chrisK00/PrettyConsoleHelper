40ff7996c230b00550eecd18697df89c024359a6
chrisk <christian.khalil200@gmail.com>
2021-09-14 11:44:24 +0200

    better mock console

--- PrettyConsoleHelper.Tests/MockPrettyConsole.cs
+++ PrettyConsoleHelper.Tests/MockPrettyConsole.cs
@@ -1,7 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PrettyConsoleHelper.Tests
 {
+    public class MockPrettyConsoleWithMultipleInputs : IPrettyConsole
+    {
+        public List<string> LinesToRead { get; set; } = new();
+
+        public PrettyConsoleOptions Options { get; init; }
+
+        public void LogError(string message, Exception ex = null)
+        {
+        }
+
+        public void LogWarning(string message, Exception ex = null)
+        {
+        }
+
+        public string ReadLine()
+        {
+            var indexOfFirstItem = 0;
+            var line = LinesToRead[indexOfFirstItem];
+            LinesToRead.RemoveAt(indexOfFirstItem);
+            return line;
+        }
+
+        public void Write(char value, int times, ConsoleColor color = ConsoleColor.White)
+        {
+        }
+
+        public void Write(object value, ConsoleColor color)
+        {
+        }
+
+        public void Write(string text)
+        {
+        }
+
+        public void Write(string text, bool prompt)
+        {
+        }
+
+        public void Write(int value, ConsoleColor color)
+        {
+        }
+
+        public void Write(string text, ConsoleColor color, bool prompt)
+        {
+        }
+
+        public void Write(int value)
+        {
+        }
+
+        public void WriteLine(object value)
+        {
+        }
+
+        public void WriteLine(object value, ConsoleColor color)
+        {
+        }
+
+        public void WriteLine(string text)
+        {
+        }
+
+        public void WriteLine(string text, ConsoleColor color)
+        {
+        }
+
+        public void WriteLine(char value, int times, ConsoleColor color = ConsoleColor.White)
+        {
+        }
+    }
+
     public class MockPrettyConsole : IPrettyConsole
     {
         public MockPrettyConsole()
